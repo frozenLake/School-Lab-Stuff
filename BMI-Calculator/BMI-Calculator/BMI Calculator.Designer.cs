88db50f07458bf38970b1a44839d9fb853074695
frozenLake <frozenLake123@gmail.com>
2022-04-20 10:59:09 -0500

    BMI Calculator

---
+++ BMI-Calculator/BMI-Calculator/BMI Calculator.Designer.cs
@@ -0,0 +1,137 @@
+﻿
+namespace BMI_Calculator
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.labelTitle = new System.Windows.Forms.Label();
+			this.labelWeight = new System.Windows.Forms.Label();
+			this.labelHeight = new System.Windows.Forms.Label();
+			this.labelResult = new System.Windows.Forms.Label();
+			this.labelMessage = new System.Windows.Forms.Label();
+			this.buttonCalculate = new System.Windows.Forms.Button();
+			this.buttonReset = new System.Windows.Forms.Button();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.textBoxWeight = new System.Windows.Forms.TextBox();
+			this.textBoxHeight = new System.Windows.Forms.TextBox();
+			this.SuspendLayout();
+			// 
+			// labelTitle
+			// 
+			resources.ApplyResources(this.labelTitle, "labelTitle");
+			this.labelTitle.Name = "labelTitle";
+			// 
+			// labelWeight
+			// 
+			resources.ApplyResources(this.labelWeight, "labelWeight");
+			this.labelWeight.Name = "labelWeight";
+			// 
+			// labelHeight
+			// 
+			resources.ApplyResources(this.labelHeight, "labelHeight");
+			this.labelHeight.Name = "labelHeight";
+			// 
+			// labelResult
+			// 
+			resources.ApplyResources(this.labelResult, "labelResult");
+			this.labelResult.Name = "labelResult";
+			this.labelResult.Click += new System.EventHandler(this.labelResult_Click);
+			// 
+			// labelMessage
+			// 
+			resources.ApplyResources(this.labelMessage, "labelMessage");
+			this.labelMessage.Name = "labelMessage";
+			// 
+			// buttonCalculate
+			// 
+			resources.ApplyResources(this.buttonCalculate, "buttonCalculate");
+			this.buttonCalculate.Name = "buttonCalculate";
+			this.buttonCalculate.UseVisualStyleBackColor = true;
+			this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+			// 
+			// buttonReset
+			// 
+			resources.ApplyResources(this.buttonReset, "buttonReset");
+			this.buttonReset.Name = "buttonReset";
+			this.buttonReset.UseVisualStyleBackColor = true;
+			this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+			// 
+			// buttonExit
+			// 
+			resources.ApplyResources(this.buttonExit, "buttonExit");
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// textBoxWeight
+			// 
+			resources.ApplyResources(this.textBoxWeight, "textBoxWeight");
+			this.textBoxWeight.Name = "textBoxWeight";
+			// 
+			// textBoxHeight
+			// 
+			resources.ApplyResources(this.textBoxHeight, "textBoxHeight");
+			this.textBoxHeight.Name = "textBoxHeight";
+			// 
+			// Form1
+			// 
+			resources.ApplyResources(this, "$this");
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.Controls.Add(this.textBoxHeight);
+			this.Controls.Add(this.textBoxWeight);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonReset);
+			this.Controls.Add(this.buttonCalculate);
+			this.Controls.Add(this.labelMessage);
+			this.Controls.Add(this.labelResult);
+			this.Controls.Add(this.labelHeight);
+			this.Controls.Add(this.labelWeight);
+			this.Controls.Add(this.labelTitle);
+			this.Name = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Label labelTitle;
+		private System.Windows.Forms.Label labelWeight;
+		private System.Windows.Forms.Label labelHeight;
+		private System.Windows.Forms.Label labelResult;
+		private System.Windows.Forms.Label labelMessage;
+		private System.Windows.Forms.Button buttonCalculate;
+		private System.Windows.Forms.Button buttonReset;
+		private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.TextBox textBoxWeight;
+		private System.Windows.Forms.TextBox textBoxHeight;
+	}
+}
+
---
+++ BMI-Calculator/BMI-Calculator/BMI Calculator.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BMI_Calculator
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+			this.Text = "BMI Calculator";
+		}
+
+		private void buttonExit_Click(object sender, EventArgs e)
+		{
+			Application.Exit();
+		}
+
+		private void labelResult_Click(object sender, EventArgs e)
+		{
+
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			labelResult.Text = "";
+			labelMessage.Text = "";
+		}
+
+		private void buttonCalculate_Click(object sender, EventArgs e)
+		{
+			try
+			{
+				if (textBoxWeight.Text == "" || textBoxHeight.Text == "")
+				{
+					labelMessage.Text = "Please enter the Weight and Height \n to calculate your BMI";
+				}
+				else
+				{
+					// Parse input as doubles
+					double weight = double.Parse(textBoxWeight.Text);
+					double height = double.Parse(textBoxHeight.Text);
+					double bmi = (weight * 703) / (height * height);
+
+					// Display the result in the result label
+					labelResult.Text = "Your BMI: " + String.Format("{0:f}", bmi);
+
+					// Check BMI and give relevant message
+					if (bmi < 18.5)
+					{
+						labelMessage.Text = "Your are under-weight. \n";
+						labelMessage.Text += "Take more nutritious food to get your ideal weight. \n";
+					}
+
+					else if (bmi >= 18.5 && bmi <= 24.9)
+					{
+						labelMessage.Text = "Good! Your weight is normal. \n";
+					}
+
+					else if (bmi >= 25 && bmi <= 29.9)
+					{
+						labelMessage.Text = "You are over-weight. \n";
+						labelMessage.Text += "Focus on diet and workout. \n";
+					}
+					else if (bmi >= 30)
+					{
+						labelMessage.Text = "You are obese. \n";
+						labelMessage.Text += "Consult with a doctor immediately. \n";
+					}
+					else
+					{
+						labelMessage.Text = "Please provide valid data to calculate BMI";
+					}
+
+					labelMessage.Text += "Stay Healthy! Happy Living!";
+				}
+			}
+
+			catch (Exception ex)
+			{
+				labelMessage.Text = ex.ToString();
+			}
+		}
+
+		private void buttonReset_Click(object sender, EventArgs e)
+		{
+			textBoxWeight.Text = "";
+			textBoxHeight.Text = "";
+			labelResult.Text = "";
+			labelMessage.Text = "";
+		}
+	}
+}
